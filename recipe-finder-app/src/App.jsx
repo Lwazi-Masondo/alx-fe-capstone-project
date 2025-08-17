b3e19185ea038ad74da673c6ee70a939d815d72d
Nolwazi Masondo <masondop96@gmail.com>
2025-08-17 17:57:13 +0200

    Initialized new recipe finder project and installed tailwindCSS

---
+++ recipe-finder-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-blue-600 text-2xl">
+        Welcome to my recipe finder app
+      </h1>
+    </>
+  );
+}
+
+export default App;
