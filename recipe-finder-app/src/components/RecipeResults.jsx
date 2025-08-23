6c628b3ad0ac8b47af88835e22730b25f8046f0a
Nolwazi Masondo <masondop96@gmail.com>
2025-08-23 03:47:54 +0200

    Added TailwindCSS styles to RecipeResults component

--- recipe-finder-app/src/components/RecipeResults.jsx
+++ recipe-finder-app/src/components/RecipeResults.jsx
@@ -18,9 +18,22 @@ function RecipeResults() {
   //User searched aand meals exist in the api
   //.map can't map over an empty array so always apply a check before.
   return (
-    <ul className="bg-amber-50">
+    <ul className="m-5 p-5 bg-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
       {meals.map((meal) => (
-        <li key={meal.idMeal}>{meal.strMeal}</li>
+        <li
+          key={meal.idMeal}
+          className="text-white text-center text-sm  w-40 h-auto m-5 rounded relative cursor-pointer border-2 border-orange-500 "
+        >
+          <img
+            src={meal.strMealThumb}
+            alt={meal.strMeal}
+            className="w-40 h-40 mx-auto rounded  drop-shadow-2xl "
+          />
+          <div className="absolute inset-x-0 bottom-0 bg-black/70 rounded-b  ">
+            <h3 className="font-bold">{meal.strMeal}</h3>
+            <p>{meal.strCategory}</p>
+          </div>
+        </li>
       ))}
     </ul>
   );
