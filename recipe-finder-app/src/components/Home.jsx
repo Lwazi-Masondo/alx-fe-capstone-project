1092a511f7835fcfbb20abb0f0047edff0899f4f
Nolwazi Masondo <masondop96@gmail.com>
2025-09-02 02:15:32 +0200

    Added animation to homepage elements

--- recipe-finder-app/src/components/Home.jsx
+++ recipe-finder-app/src/components/Home.jsx
@@ -1,13 +1,13 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-center font-bold text-2xl text-orange-500 my-10 mx-10">
+      <h1 className="text-center font-bold text-2xl text-orange-500 my-10 mx-10 appear">
         Your taste buds called, they’re ready to plan the week!
       </h1>
       <img
         src="/randomfood.png"
         alt="Food"
-        className="w-44 md:w-80 lg:w-96 m-auto mt-10 mb-20 rounded-xl"
+        className="w-44 md:w-80 lg:w-96 m-auto mt-10 mb-20 rounded-xl scale-in "
       />
     </div>
   );
